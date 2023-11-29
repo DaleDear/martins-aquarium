52455925d99363fba4f33e61886345ea7942f6ef
Dale Dear <kristendaledear@gmail.com>
2023-11-29 14:39:15 -0600

    commit fish tips locations files

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -6,23 +6,24 @@ export const FishList = () => {
     const fishes = getFish()
 
     // Start building a string filled with HTML syntax
-    const htmlString = <article class="fishList">
+    const htmlString = '<article class="fishList">'
 
     // Create HTNL representations of each fish here
     for (const fish of fishes) {
 
         // Why is there a backtick used for this string?
-        htmlString += <section class="fish card">
+        htmlString == `<section class="fishCard">
             <div><img  class="fish__image image--card" src="${fish.image}" /></div>
             <div class="fish__name">${fish.name}</div>
+            <div class="fish__type">${fish.type}</div>
             <div class="fish__species">${fish.species}</div>
-            <div class="fish__length">${fish.length}</div>
-            <div class="fish__location">${fish.location}</div>
+            <div class="fish__size">${fish.size}</div>
+            <div class="fish__harvestLocation">${fish.harvestLocation}</div>
             <div class="fish__diet">${fish.food}</div>
         </section>
-
+`
     }
-    htmlString += </article>
+    htmlString == `</article>`
 
     return htmlString
 }
--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,22 @@ for (const fish of allFish) {
     console.log(fish)
 }
 
+    //for (let i = 0; i < fish.length; i++) {
+
+      
+
 // Import the FishList function from the correct module
 import { FishList } from './FishList.js'
 
-/*
-    What is the CSS selector for the element where you
-    want to display the fish?
+const fishCard = getFish(); {
+    /*
+        What is the CSS selector for the element where you
+        want to display the fish?
+    
+        Use . for elements with a "class" attribute
+        Use # for elements with an "id" attribute
+     */
+}    
+const parentHTMLElement = document.querySelectorAll('.fishCard');
 
-    Use . for elements with a "class" attribute
-    Use # for elements with an "id" attribute
- */
-const parentHTMLElement = document.querySelector(.mainComponent)
-
-
-parentHTMLElement.innerHTML = FishList()
+parentHTMLElement.innerHTML = FishList();
